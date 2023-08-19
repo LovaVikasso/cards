5e97d08ca7862aad86c109754363b9e0613da38a
victoriapavlova <victoria.pavlova7@gmail.com>
2023-08-19 21:29:56 +0300

    add polymorphic button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { Button } from './components/ui'
+
 export function App() {
-  return <div>Hello world</div>
+  return (
+    <div>
+      Hello world
+      <Button variant="primary" as={'button'}>
+        Button
+      </Button>
+      <Button variant="primary" as={'a'} href="https://www.google.ru/">
+        href
+      </Button>
+    </div>
+  )
 }
---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,25 @@
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+
+import s from './button.module.scss'
+
+export type ButtonProps<T extends ElementType = 'button'> = {
+  as?: T
+  children: ReactNode
+  variant?: 'primary' | 'secondary' | 'tertiary' | 'link'
+  fullWidth?: boolean
+  className?: string
+} & ComponentPropsWithoutRef<T> //способ достать дефолтные пропсы любого стандартного элемента, исп. для рефа
+
+export const Button = <T extends ElementType = 'button'>(
+  props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
+  //С помощью Omit мы убираем из пропсов переданного компонента все пропсы, которые уже есть в наших кастомных пропсах, тем самым избегая коллизий.
+) => {
+  const { variant = 'primary', fullWidth, className, as: Component = 'button', ...rest } = props
+
+  return (
+    <Component className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest} />
+  )
+}
+// ${className} если будет уникальная кнока не по стандарту, перезаишет наши стили
+// {s[variant]} применем стиль s в зависимоти от варианта, который переменная
+// ${fullWidth ? s.fullWidth : '' если кнопка на всю длину
